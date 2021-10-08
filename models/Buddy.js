092e270a1455ab197965c11a6dd9996e5ef40141
ayu1298 <theinvincible375@gmail.com>
2021-10-08 12:34:22 +0530

    Server Side Configuration

---
+++ models/Buddy.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const buddySchema= new Schema({
+      
+    username:
+    {
+        type : String,
+        required: true,
+        unique:true
+    },
+    email:
+    {
+        type : String,
+        required: true,
+        unique:true
+    },
+    password:
+    {
+        type:String,
+        required:true,
+        
+    },
+    sessionID:
+    {
+        type:String
+    }
+});
+module.exports=mongoose.model("Buddy",buddySchema);
