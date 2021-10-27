a91227dbd4507748d5abd9bf904b729d03d86f68
OmThakur92 <omkumarbhu@gmail.com>
2021-10-28 01:37:16 +0530

    Added the login and homepage  v1

--- app.js
+++ app.js
@@ -3,28 +3,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-
-
 const app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-
-
-
 app.get("/", function (req, res) {
-    res.render("home");
+  res.render("home");
 });
 
+app.post("/", function (req, res) {});
 
-app.post("/", function (req, res) {
+app.get("/login", function (req, res) {
+  res.render("login");
 });
-
-
 
 app.listen(3000, function () {
   console.log("Server started at the port 3000.");
