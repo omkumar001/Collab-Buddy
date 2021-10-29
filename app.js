c26c9f53cfa8b0b9fccf1a3576969ff95f485023
OmThakur92 <omkumarbhu@gmail.com>
2021-10-29 21:00:34 +0530

    Contains the dashboard , search bar , homepage and login/signup files

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app = express();
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 
-const session = require("express-session");
-const MongoDBSession = require("connect-mongodb-session")(session);
+// const session = require("express-session");
+// const MongoDBSession = require("connect-mongodb-session")(session);
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/uploads"));
 app.set("view engine", "ejs");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 
 app.use(
   bodyparser.urlencoded({
@@ -18,42 +18,42 @@ app.use(
   })
 );
 
-const BuddyModel = require("./models/Buddy");
-const BuddyApplication = require("./models/BuddyApplication");
+// const BuddyModel = require("./models/Buddy");
+// const BuddyApplication = require("./models/BuddyApplication");
 
-const mongoURI = "mongodb://localhost:27017/buddyDB";
+// const mongoURI = "mongodb://localhost:27017/buddyDB";
 
-mongoose
-  .connect("mongodb://localhost:27017/buddyDB", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+// mongoose
+//   .connect("mongodb://localhost:27017/buddyDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
 
-  .then((res) => {
-    console.log("Connected Mongo");
-  });
+//   .then((res) => {
+//     console.log("Connected Mongo");
+//   });
 
-// Create store for session
+// // Create store for session
 
-const store = new MongoDBSession({
-  uri: mongoURI,
-  collection: "mySessions",
-});
+// const store = new MongoDBSession({
+//   uri: mongoURI,
+//   collection: "mySessions",
+// });
 
-app.use(
-  session({
-    secret: "key that will sign cookie",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
-);
+// app.use(
+//   session({
+//     secret: "key that will sign cookie",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: store,
+//   })
+// );
 
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+// app.use(
+//   express.urlencoded({
+//     extended: true,
+//   })
+// );
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -68,6 +68,26 @@ app.get("/login", function (req, res) {
 app.get("/shome", function (req, res) {
   res.render("shome");
 });
+
+app.get("/shome", function (req, res) {
+    res.render("shome");
+  });
+
+  app.get("/profile", function (req, res) {
+    res.render("profile");
+  });
+
+  app.post("/profile", function (req, res) {
+    res.redirect("/");
+  });
+
+  app.get("/application", function (req, res) {
+    res.render("application");
+  });
+
+  app.post("/application", function (req, res) {
+    res.redirect("/");
+  });
 
 app.listen("3000", function (req, res) {
   console.log("Connected successfully to the server");
